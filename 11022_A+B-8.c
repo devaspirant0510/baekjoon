4dc6e115846d056463ac83517314f9550af5ef69
devaspirant0510 <dltmdgh2@sonline20.sen.go.kr>
2020-12-09 16:37:21 +0900

    add baekjoon A+B-8 11022

---
+++ 11022_A+B-8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int n,i,a,b;
+	scanf("%d",&n);
+	for (i=1; i<=n; i++){
+		scanf("%d %d",&a,&b);
+		printf("Case #%d: %d + %d = %d\n",i,a,b,a+b);
+	}
+	return 0;
+}
