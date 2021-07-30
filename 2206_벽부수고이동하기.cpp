cf28deaa84a49b1eb9f55a73c8beed79fff7b8e2
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-07-30 21:26:43 +0900

    modify baekjoon 2206 벽부수고 이동하기

--- 2206_벽부수고이동하기.cpp
+++ 2206_벽부수고이동하기.cpp
@@ -1,33 +1,34 @@
 #include <iostream>
+#include <cstring>
 #include <cstdio>
 #include <queue>
-#include <cstring>
+
+#define MAX 1001
 
 using namespace std;
-int board[1001][1001] = { 0, };
-int cBoard[1001][1001][2] = { 0, }; // 벽뚫기 여부
 
-typedef struct{
-	int y;
-	int x;
-} dxy;
+int board[MAX][MAX];
+int cBoard[MAX][MAX][2];
 
-
-// 왼쪽 → 오른쪽 → 위쪽 → 아래쪽
-dxy movePos[4] = { {1,0},{0,1},{-1,0},{0,-1} };
+// N 은 y축 M 은 x 축
 int N, M;
 
+typedef struct {
+	int y;
+	int x;
+} Direction;
+Direction dir[4] = { {0,1},{0,-1},{1,0},{-1,0} };
 void input();
-void output();
-int solution();
+void solution();
+bool isMove(int y,int x);
+void log();
 void outca();
-bool check(int y, int x);
-// N 은 y 축 M 은 x 축
-// 0은 갈수있는 길 1은 벽
+
+
 int main(void) {
 	input();
-	cout << solution() << endl;
-	//output();
+	solution();
+	return 0;
 }
 void input() {
 	cin >> N >> M;
@@ -37,52 +38,71 @@ void input() {
 		}
 	}
 }
-int solution() {
-	queue <pair<pair<int, int>, int> > que; // y x 좌표 벽뚫기 여부
-	que.push({ {0,0},1 });
-	cBoard[0][0][1] = 1; // 초기값은 벽을 뚫을수 있는 기회가 있기때문에 0,0,1 
-	while (!que.empty()) {
-		int y = que.front().first.first;
-		int x = que.front().first.second;
-		int block = que.front().second;
-		que.pop();
-		// 종료 조건 : 현재 내위치가 (M-1,N-1) 에 있을때
-		if ( y == N-1 && x==M-1) {
-			return cBoard[y][x][block];
+void solution() {
+	// {{y,x},벽부순 여부(1은 안부숨 0은 부숨)
+	queue<pair<Direction, int>> q;
+	cBoard[0][0][1] = 1;
+	q.push({ {0,0,},1 });
+
+	while (!q.empty()) {
+		int y = q.front().first.y;
+		int x = q.front().first.x;
+		int block = q.front().second;
+		q.pop();
+		if (y == N-1 && x == M-1) {
+			cout << cBoard[y][x][block];
+			return;
 		}
 		for (int i = 0; i < 4; i++) {
-			int goY = y+movePos[i].y;
-			int goX = x+movePos[i].x;
+			int dx = dir[i].x + x;
+			int dy = dir[i].y + y;
+			if (isMove(dy, dx)) {
+				if (block && board[dy][dx] == 1) {
+					cBoard[dy][dx][block - 1] = cBoard[y][x][block] + 1;
+					q.push({ {dy,dx},block - 1 });
+					//outca();
 
-			// 움직일수있는지 체크 (벽이 있는지 체크는 안함)
-			if (check(goY, goX)) {
-				// 벽이 있고, 벽을 한번도 부수지 않았을시 
-				if (board[goY][goX] == 1 && block) {
-					// 방문처리
-					cBoard[goY][goX][block - 1] = cBoard[y][x][block] + 1;
-					que.push({ {goY,goX},block-1 });
+				}if (cBoard[dy][dx][block]==0 && board[dy][dx] == 0) {
+					cBoard[dy][dx][block] = cBoard[y][x][block] + 1;
+					q.push({ {dy,dx},block });
+					//outca();
+
 				}
-				// 갈수 있는길이고, 방문처리가 안된곳
-				else if (board[goY][goX] == 0 && cBoard[goY][goX][block]==0) {
-					cBoard[goY][goX][block] = cBoard[y][x][block]+1;
-					que.push({ {goY,goX},block });
-				}
+
 			}
 		}
 	}
-	return -1;
+	
+	cout << -1;
+
 }
-void output() {
+bool isMove(int y,int x) {
+	if (0 <= y && y < N && 0 <= x && x < M) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+void log() {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			cout << board[i][j];
+			cout << board[i][j] << "   ";
 		}
 		cout << endl;
 	}
+
+
 }
 void outca() {
 	cout << endl;
 	for (int i = 0; i < 2; i++) {
+		if (i == 0) {
+			cout << "벽 부순 배열" << endl;
+		}
+		else {
+			cout << "벽 안부순 배열" << endl;
+		}
 		for (int j = 0; j < N; j++) {
 			for (int k = 0; k < M; k++) {
 				cout << cBoard[j][k][i];
@@ -93,6 +113,3 @@ void outca() {
 	}
 }
 
-bool check(int y, int x) {
-	return (y >= 0 && y <N && x >= 0 && x < M);
-}
